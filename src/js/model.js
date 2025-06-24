4ce4609c7bbfea537d9bb118d0722a81669a9c45
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-06-24 13:19:48 +0300

    added helpers and configuration files

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,13 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -20,6 +20,6 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
   } catch (error) {
-    alert(error);
+    console.error(`${error} 💣💣💣💣`);
   }
 };
