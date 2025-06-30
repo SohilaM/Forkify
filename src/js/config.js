d3e36a8cd7a0a7ccc4eb33b3a90c5e5b5d6f0d41
SohilaM <111076455+SohilaM@users.noreply.github.com>
2025-06-30 23:11:25 +0300

    implement sending recipes to API and rendering them

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a50fa1eb-36db-4bc8-a0a3-d2b0d6388287';
+export const MODAL_CLOSE_SEC = 2.5;
